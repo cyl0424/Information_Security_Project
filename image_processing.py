80c364cedf164eea788477e271e84ca6b093f974
Chu Yulim <20102124@g.seoultech.ac.kr>
2024-05-31 16:00:05 +0900

    modify approx func

--- image_processing.py
+++ image_processing.py
@@ -76,3 +76,30 @@ def process_image_rgb(image_path, pool_size, stride):
         print(f"Error processing image: {e}")
         raise
 
+def process_image_approx(image_path, pool_size, stride):
+    try:
+        image = Image.open(image_path).convert('L')  # Convert to grayscale
+        image_array = np.array(image)
+
+        # Perform max pooling
+        pooled_image_array = utils.approx_max_pooling(image_array, pool_size, stride)
+
+        # Ensure the array is real
+        if np.iscomplexobj(pooled_image_array):
+            pooled_image_array = np.real(pooled_image_array)
+
+        # Calculate the dimensions of the pooled image without padding
+        original_h, original_w = image_array.shape
+        pooled_h = (original_h - pool_size) // stride + 1
+        pooled_w = (original_w - pool_size) // stride + 1
+
+        # Slice the pooled_image_array to remove padding
+        sliced_pooled_image_array = pooled_image_array[:pooled_h, :pooled_w]
+
+        # Convert to image
+        pooled_image = Image.fromarray(sliced_pooled_image_array.astype('uint8'))
+
+        return sliced_pooled_image_array, pooled_image
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        raise
